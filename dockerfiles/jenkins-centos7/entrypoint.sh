0958e1d1570bd90202cc927d90972fed9a32e7d7
wanghaohao <wanghaohao@pagoda.com.cn>
2022-07-02 15:25:16 +0800

    jenkins增加jmx包，以暴露jvm监控数据。

--- dockerfiles/jenkins-centos7/entrypoint.sh
+++ dockerfiles/jenkins-centos7/entrypoint.sh
@@ -9,4 +9,5 @@
     --httpPort=8080 \
     --debug=5 \
     --handlerCountMax=100 \
-    --handlerCountMaxIdle=20
+    --handlerCountMaxIdle=20 \
+    -javaagent:/lib/jmx_prometheus_javaagent.jar=60030:/etc/jmx-cfg.yml
