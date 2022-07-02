814a5cf7e2b7b5f6507bde21793ac8ed95888de2
wanghaohao <wanghaohao@pagoda.com.cn>
2022-07-02 16:56:17 +0800

    调整启动脚本

--- dockerfiles/jenkins-centos7/entrypoint.sh
+++ dockerfiles/jenkins-centos7/entrypoint.sh
@@ -3,11 +3,11 @@
 /usr/bin/java \
     -Djava.awt.headless=true \
     -DJENKINS_HOME=/data \
-    -jar /usr/lib/jenkins/jenkins.war \
     --logfile=/data/jenkins.log \
     --webroot=/data/war \
     --httpPort=8080 \
     --debug=5 \
     --handlerCountMax=100 \
     --handlerCountMaxIdle=20 \
-    -javaagent:/lib/jmx_prometheus_javaagent.jar=60030:/etc/jmx-cfg.yml
+    -javaagent:/lib/jmx_prometheus_javaagent.jar=60030:/etc/jmx-cfg.yml \
+    -jar /usr/lib/jenkins/jenkins.war
