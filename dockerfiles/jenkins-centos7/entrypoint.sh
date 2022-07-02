ccddf1cca10c67192ac73a2a698dfc4fd2966a93
wanghaohao <wanghaohao@pagoda.com.cn>
2022-07-02 17:11:04 +0800

    将entrypoint.sh变量化

--- dockerfiles/jenkins-centos7/entrypoint.sh
+++ dockerfiles/jenkins-centos7/entrypoint.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-/usr/bin/java \
-    -Djava.awt.headless=true \
-    -DJENKINS_HOME=/data \
-    --logfile=/data/jenkins.log \
-    --webroot=/data/war \
-    --httpPort=8080 \
-    --debug=5 \
-    --handlerCountMax=100 \
-    --handlerCountMaxIdle=20 \
-    -javaagent:/lib/jmx_prometheus_javaagent.jar=60030:/etc/jmx-cfg.yml \
-    -jar /usr/lib/jenkins/jenkins.war
+/usr/bin/java -Xms${JAVA_HEAP_XMS} -Xmx${JAVA_HEAP_XMX} ${JAVA_JMX_OPTS} ${JAVA_OPTS} $@
