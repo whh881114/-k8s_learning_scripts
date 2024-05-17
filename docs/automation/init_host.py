5d8dc822d20bd7ee18826a35a9cbf8a6df745b1f
汪浩浩 <roy_wong_coding@163.com>
2024-05-17 18:36:05 +0800

    增加脚本内容

--- docs/automation/init_host.py
+++ docs/automation/init_host.py
@@ -8,19 +8,28 @@ import subprocess
 
 from colorama import Fore, Back, Style
 
+print("Content-type:text/html")
+print("")
 
 # 创建 FieldStorage 的实例化
 form = cgi.FieldStorage()
 
 # 获取数据
-site_name = form.getvalue('name')
-site_url  = form.getvalue('url')
+id = form.getvalue("id")
+hostname  = form.getvalue("hostname")
+ip  = form.getvalue("ip")
+ansible_hostgroup = "-".join(hostname.split("-")[0:-1])
 
-print("Content-type:text/html")
-print("")
-print("name: %s, url: %s" % (site_name, site_url))
+print(Fore.BLUE + "[INFO] 初始化主机信息：ID: %s, HOSTNAME: %s, IP: %s, ANSIBLE_HOSTGROUP: %s" %
+      (id, hostname, ip, ansible_hostgroup), Style.RESET_ALL)
 
-print("开始初始化主机")
+# 默认使用ansible_hostgroup命名的playbook，如test-bigdata.yaml，如果没有则使用default.yaml文件进行初始化。
+ansible_default_playbook = "default.yaml"
+ansible_hostgroup_playbook = ansible_hostgroup + ".yaml"
+
+cmd_exist_default_playbook = "test -f %s" % ansible_default_playbook
+cmd_exist_ansible_hostgroup_playbook = "test -f %s" % ansible_default_playbook
+
 
 test_cmd = "ansible 192.168.255.251 -u root -m shell -a 'touch /tmp/init_host.py--$$.txt'"
 p = subprocess.Popen(test_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
